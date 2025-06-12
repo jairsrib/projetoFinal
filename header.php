182e455b7641786403a3475f12513ec4793efc50
leoverardo <leonardomoura080904@gmail.com>
2025-06-11 21:04:38 -0300

    header pronto

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/reset.css">
+    <link rel="stylesheet" href="assets/header.css">
+    <title>Document</title>
+</head>
+<header>
+    <div class="container">
+        <div class="btn-menu">
+        <button class="btn-hamburger btn-toggle-menu" aria-label="Menu Hamburger">
+                <svg width="32" height="32" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 1H17" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M1 14.9087H17" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M1 8H17.0001" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </button>
+        </div>
+
+        <div class="logo">
+        <img src="img/logo.png" alt="">
+        <div class="titulo">
+        <h1>Caiu o <span>Servidor</span></h1>
+        <h2>notícias do mundo do games </h2>
+        </div>
+        </div>
+        <nav>
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="about.php">Noiticas</a></li>
+                    <li><a href="blog.php">Autores</a></li>
+                    <li><a href="contact.php">Contato</a></li>
+                </ul>
+        </nav>
+    </div>
+</header>
+</html>
