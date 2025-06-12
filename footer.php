73194478242b557d52360018e6c72c4989f8a6a5
leoverardo <leonardomoura080904@gmail.com>
2025-06-11 21:58:54 -0300

    finalizado identidade da tela de login, falta a responsividade

---
+++ footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/footer.css">
+    <link rel="stylesheet" href="assets/reset.css">
+</head>
+<footer class="footer">
+            <div class="footer-top">
+                <div class="footer-col">
+                    <h3><span>Contato</span></h3>
+                    <ul>
+                        <li>Telefone: (99) 9999-9999</li>
+                        <li>Email: Caiuoservidornoticias@email.com</li>
+                        <li>Endereço: Rua dos Games 141812</li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h3><span>Informações</span></h3>
+                    <ul class="social">
+                        <li><a href="">Noticías</a></li>
+                        <li><a href="">Autores</a></li>
+                        <li><a href="">Cadastrar-se</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h3><span>Redes Sociais</span></h3>
+                    <ul class="social">
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">X</a></li>
+                    </ul>
+                </div>
+                <div id="modalContainer" class="modal hidden"></div>
+                <div class="footer-col">
+                    <a href="#top" class="back-to-top">Voltar ao topo</a>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p>&copy; 2025 Caiu o Servidor</p>
+                <div class="footer-links">
+                    <a href="#">Política de Privacidade</a>
+                    <a href="#">Desenvolvido por Leonardo Verardo e Jair de Souza</a>
+                </div>
+            </div>
+        </footer>
+</html>
