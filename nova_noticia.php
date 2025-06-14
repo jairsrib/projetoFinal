35d3370ec69cd4f2dbc72c80ce70e909482e0aea
jairsrib <leonardomoura080904@gmail.com>
2025-06-13 21:01:12 -0300

    nova noticia

--- nova_noticia.php
+++ nova_noticia.php
@@ -4,40 +4,48 @@ include_once './classes/Usuario.php';
 include_once './config/config.php';
 include_once './classes/Noticia.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
- $noticia = new Noticia($db);
- $titulo = $_POST['titulo'];
- $texto = $_POST['texto'];
- $data = $_POST['data'];
- $autor_id = $_SESSION['usuario_id'] ?? null;
- $imagem = $_POST['imagem'];
- $noticia->criar($titulo, $texto, $data, $autor_id, $imagem);
- header('Location: portal.php');
- exit();
+    date_default_timezone_set(timezoneId: 'America/Sao_Paulo');
+    $noticia = new Noticia($db);
+    $titulo = $_POST['titulo'];
+    $texto = $_POST['texto'];
+    $data = $_POST['data'];
+    $autor_id = $_SESSION['usuario_id'] ?? null;
+    $imagem = $_POST['imagem'];
+    $currentDateTime = date('Y-m-d H:i:s');
+    $noticia->criar($titulo, $texto, $currentDateTime, $autor_id, $imagem);
+    header('Location: portal.php');
+    exit();
 }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
- <meta charset="UTF-8">
- <title>Adicionar Notícia</title>
-</head>
-<body>
- <h1>Adicionar Notícia</h1>
- <form method="POST">
 
-  <label for="nome">titulo:</label>
-  <input type="text" name="titulo" required>
-  <br><br>
-  <label for="fone">noticia:</label>
-  <input type="text" name="not" required>
-  <br><br>
-  <label for="email">data:</label>
-  <input type="date" name="data" required>
-  <br><br>
-  <label for="senha">imagem:</label>
-  <input type="file" name="imagem" required>
-  <br><br>
-  <input type="submit" value="Adicionar">
- </form>
+<head>
+    <meta charset="UTF-8">
+    <title>Adicionar Notícia</title>
+</head>
+
+<body>
+    <h1>Adicionar Notícia</h1>
+    <form method="POST">
+
+        <label for="titulo">titulo:</label>
+        <input type="text" name="titulo" required>
+        <br><br>
+        <label for="noticia">noticia:</label>
+        <input type="text" name="not" required>
+        <br><br>
+        <label for="categoria">categoria</label>
+        <select name="categoria" required>
+            <option value="noticia">noticia</option>
+            <option value="jogo">jogo</option>
+            <option value="evento">evento</option>
+            <br><br>
+            <label for="imagem">imagem:</label>
+            <input type="file" name="imagem">
+            <br><br>
+            <input type="submit" value="Adicionar">
+    </form>
 </body>
+
 </html>
