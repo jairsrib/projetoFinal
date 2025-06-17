aa4ea4ecdf0ddd247f0d15ed26a89dbce929f55c
leoverardo <leonardomoura080904@gmail.com>
2025-06-16 21:47:20 -0300

    Dashboard em andamento

--- dashboard.php
+++ dashboard.php
@@ -23,10 +23,90 @@
         </div>
         <div class="row-3">
         <div class="col-12">
-            <img src="./img/SobreE-Sports.png" alt="" width="450px" height="450px">
+            <section>
+    <div class="container">
+        <div class="carousel">
+            <input type="radio" name="slides" checked="checked" id="slide-1">
+            <input type="radio" name="slides" id="slide-2">
+            <input type="radio" name="slides" id="slide-3">
+            <input type="radio" name="slides" id="slide-4">
+            <input type="radio" name="slides" id="slide-5">
+            <input type="radio" name="slides" id="slide-6">
+            <ul class="carousel__slides">
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="img/SobreE-Sports.png" alt="">
+                        </div>
+                        <figcaption>
+                            <span class="credit">Sobre E-Sports</span>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="img/profissionalismo.png" alt="">
+                        </div>
+                        <figcaption>
+                            <span class="credit">Profissionalismo</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="img/olimpiadas.png" alt="">
+                        </div>
+                        <figcaption>
+                            
+                            <span class="credit">Olimpiadas</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="img/lucrativo.png" alt="">
+                        </div>
+                        <figcaption>
+                            
+                            <span class="credit">Lucrativo</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="img/competitivo.png" alt="">
+                        </div>
+                        <figcaption>
+                            
+                            <span class="credit">Competitivo</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+            </ul>    
+            <ul class="carousel__thumbnails">
+                <li>
+                    <label for="slide-1"><img src="img/SobreE-Sports.png" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-2"><img src="img/profissionalismo.png" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-3"><img src="img/olimpiadas.png" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-4"><img src="img/lucrativo.png" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-5"><img src="img/competitivo.png" alt=""></label>
+                </li>
+            </ul>
         </div>
-        <div class="col-12">
-            <img src="./img/profissionalismo.png" alt="" width="450px" height="450px">
+    </div>
+</section>
         </div>
         </div>
     </div>
@@ -74,4 +154,144 @@ body {
     display: flex;
     justify-content: center;
 }
+
+
+
+
+section {
+    padding: 50px 0;
+}
+
+.container {
+    max-width: 1044px;
+    margin: 0 auto;
+    padding: 0 20px;
+}
+
+.carousel {
+    position: relative;
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto 22px auto;
+}
+
+.carousel input[type="radio"] {
+    display: none;
+}
+
+.carousel__slides {
+    display: flex;
+    width: 500%;
+    transition: margin-left 0.5s ease;
+    margin-left: 0;
+    padding: 0;
+    list-style: none;
+    overflow: hidden;
+    border-radius: 10px;
+    background: #171717;
+}
+
+.carousel__slide {
+    width: 100%;
+    flex: 0 0 100%;
+    box-sizing: border-box;
+    color: #fff;
+    text-align: center;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.carousel__slide figure {
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.carousel__slide img {
+    width: 100%;
+    max-width: 450px;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 10px;
+    margin: 0 auto;
+}
+
+.carousel__slide figcaption {
+    margin-top: 15px;
+    font-size: 1em;
+    color: #fff;
+}
+
+.carousel__slide .credit {
+    color: #aaa;
+    font-size: 0.9em;
+}
+
+.carousel__thumbnails {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    margin-top: 20px;
+    list-style: none;
+    padding: 0;
+}
+
+.carousel__thumbnails li {
+    border-radius: 5px;
+    overflow: hidden;
+    border: 2px solid transparent;
+    transition: border 0.3s;
+}
+
+.carousel__thumbnails img {
+    width: 80px;
+    height: 60px;
+    object-fit: cover;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: box-shadow 0.3s;
+}
+
+.carousel input#slide-1:checked ~ .carousel__slides {
+    margin-left: 0%;
+}
+.carousel input#slide-2:checked ~ .carousel__slides {
+    margin-left: -100%;
+}
+.carousel input#slide-3:checked ~ .carousel__slides {
+    margin-left: -200%;
+}
+.carousel input#slide-4:checked ~ .carousel__slides {
+    margin-left: -300%;
+}
+.carousel input#slide-5:checked ~ .carousel__slides {
+    margin-left: -400%;
+}
+
+.carousel input#slide-1:checked ~ .carousel__thumbnails li:nth-child(1),
+.carousel input#slide-2:checked ~ .carousel__thumbnails li:nth-child(2),
+.carousel input#slide-3:checked ~ .carousel__thumbnails li:nth-child(3),
+.carousel input#slide-4:checked ~ .carousel__thumbnails li:nth-child(4),
+.carousel input#slide-5:checked ~ .carousel__thumbnails li:nth-child(5) {
+    border: 2px solid #FF084B;
+}
+
+@media (max-width: 700px) {
+    .carousel {
+        max-width: 100%;
+    }
+    .carousel__slide img {
+        max-width: 100%;
+        height: 180px;
+    }
+    .carousel__thumbnails img {
+        width: 50px;
+        height: 35px;
+    }
+}
+
 </style>
